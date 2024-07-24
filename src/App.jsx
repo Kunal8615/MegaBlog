2e3eda045f1a100e6c45b2e466965fea8338aec8
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-07-24 16:05:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPRIWITE_URL);
+
+  return (
+    <>
+    <h1> App right</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conft = {
+    appwriteUrl :String(import.meta.env.VITE_APPRIWITE_URL),
+    appwriteprojectId :String(import.meta.env.VITE_PROJECT_ID),
+    appwritedatabaseId :String(import.meta.env.VITE_DATABASE_ID),
+    appwritecollectionId :String(import.meta.env.VITE_COLLECTION_ID),
+    appwritebukectId :String(import.meta.env.VITE_BUCKET_ID)
+
+}
+
+export default conft
